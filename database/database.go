8bc23c667e62802fdb2bad7404c82b703e795678
Pedro Tashima <tithe.coup0l@icloud.com>
2026-05-07 00:10:49 -0300

    merge sqlite and remove mock db

--- database/database.go
+++ database/database.go
@@ -3,6 +3,11 @@ package database
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Database interface {
@@ -19,71 +24,172 @@ type Database interface {
 }
 
 type LogEntry struct {
-	Term   int    `json:"term"`
-	Index  int    `json:"index"`
-	Action string `json:"action"`
-	Key    string `json:"key"`
-	Value  string `json:"value"`
+	Term  int
+	Index int
+	Entry []byte
 }
 
-type MockDB struct {
-	logs []LogEntry
-	raft map[string]string
+type SQLite struct {
+	location string
+	db       *sql.DB
 }
 
-func NewMockDB() *MockDB {
-	return &MockDB{
-		logs: []LogEntry{},
-		raft: map[string]string{},
+func NewSQLite(location string) (*SQLite, error) {
+	db := &SQLite{location: location}
+	if err := db.open(); err != nil {
+		return nil, err
 	}
-}
-
-func (m *MockDB) open() error {
-	return nil
-}
-
-func (m *MockDB) migrate() error {
-	return nil
-}
-
-func (m *MockDB) SetRaftValue(key, value string) error {
-	m.raft[key] = value
-	return nil
-}
-
-func (m *MockDB) GetRaftValue(key string) (string, error) {
-	value, found := m.raft[key]
-	if !found {
-		return "", sql.ErrNoRows
+	if err := db.migrate(); err != nil {
+		return nil, err
 	}
-	return value, nil
+	return db, nil
 }
 
-func (m *MockDB) AppendLogs(logs []LogEntry) error {
-	m.logs = append(m.logs, logs...)
-	return nil
-}
-
-func (m *MockDB) DeleteLogsFromIndex(idx int) error {
-	m.logs = m.logs[:idx]
-	return nil
-}
-
-func (m *MockDB) GetLogs() ([]LogEntry, error) {
-	return m.logs, nil
-}
-
-func (m *MockDB) GetLog(idx int) (LogEntry, error) {
-	if len(m.logs) <= idx {
-		return LogEntry{}, sql.ErrNoRows
+func (d *SQLite) open() error {
+	db, err := sql.Open("sqlite3", d.location)
+	if err != nil {
+		return err
 	}
-	return m.logs[idx], nil
-}
-
-func (m *MockDB) CountLogs() (int, error) {
-	return len(m.logs), nil
-}
-
-func (m *MockDB) Close() error {
+	d.db = db
 	return nil
+}
+
+func (d *SQLite) migrate() error {
+	migrations := []string{
+		`CREATE TABLE IF NOT EXISTS 'raft' (
+			id INTEGER NOT NULL PRIMARY KEY,
+			key TEXT NOT NULL UNIQUE,
+			value TEXT NOT NULL
+		);`,
+		`CREATE TABLE IF NOT EXISTS 'logs' (
+			id INTEGER NOT NULL PRIMARY KEY,
+			idx INTEGER NOT NULL,
+			term INTEGER NOT NULL,
+			entry BLOB NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		);`,
+	}
+
+	for _, migration := range migrations {
+		if _, err := d.db.Exec(migration); err != nil {
+			return fmt.Errorf("failed to apply migration: %w", err)
+		}
+	}
+	return nil
+}
+
+func (d *SQLite) SetRaftValue(key, value string) (err error) {
+	stmt, err := d.db.Prepare("INSERT INTO raft(key, value) VALUES(?, ?) ON CONFLICT (key) DO UPDATE SET value=?")
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if closeErr := errors.Join(stmt.Close()); closeErr != nil {
+			err = errors.Join(err, closeErr)
+		}
+	}()
+
+	_, err = stmt.Exec(key, value, value)
+	return err
+}
+
+func (d *SQLite) GetRaftValue(key string) (string, error) {
+	var value string
+	err := d.db.QueryRow("SELECT value FROM raft WHERE key = ?", key).Scan(&value)
+	return value, err
+}
+
+func (d *SQLite) AppendLogs(logs []LogEntry) (err error) {
+	if len(logs) < 1 {
+		return nil
+	}
+	var sb strings.Builder
+	args := []any{}
+
+	if _, err := sb.WriteString("INSERT INTO logs(term, entry, idx) VALUES"); err != nil {
+		return err
+	}
+
+	for i, log := range logs {
+		if i != 0 {
+			if _, err := sb.WriteString(","); err != nil {
+				return err
+			}
+		}
+		if _, err := sb.WriteString("(?, ?, ?, ?)"); err != nil {
+			return err
+		}
+
+		args = append(args, log.Term, log.Entry, log.Index)
+	}
+	stmt, err := d.db.Prepare(sb.String())
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if closeErr := errors.Join(stmt.Close()); closeErr != nil {
+			err = errors.Join(err, closeErr)
+		}
+	}()
+
+	_, err = stmt.Exec(args...)
+	return err
+}
+
+func (d *SQLite) DeleteLogsFromIndex(idx int) (err error) {
+	query := "DELETE FROM logs WHERE idx > ?"
+
+	_, err = d.db.Exec(query, idx)
+	if err != nil {
+		return fmt.Errorf("failed to delete logs from index: %w", err)
+	}
+
+	return nil
+}
+
+func (d *SQLite) GetLogs() (logs []LogEntry, err error) {
+	rows, err := d.db.Query("SELECT term, entry, idx FROM logs;")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		log := LogEntry{}
+		err = rows.Scan(&log.Term, &log.Entry, &log.Index)
+		if err != nil {
+			return nil, err
+		}
+		logs = append(logs, log)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if closeErr := errors.Join(rows.Close()); closeErr != nil {
+			err = errors.Join(err, closeErr)
+		}
+	}()
+
+	return logs, err
+}
+
+func (d *SQLite) GetLog(idx int) (log LogEntry, err error) {
+	var term int
+	var entry []byte
+	err = d.db.QueryRow("SELECT term, entry FROM logs WHERE idx = ?", idx).Scan(&term, &entry)
+	log.Entry = entry
+	log.Term = term
+	log.Index = idx
+	return log, err
+}
+
+func (d *SQLite) CountLogs() (logCount int, err error) {
+	err = d.db.QueryRow("SELECT COUNT(*) FROM logs").Scan(&logCount)
+	return logCount, err
+}
+
+func (d *SQLite) Close() error {
+	return d.db.Close()
 }
