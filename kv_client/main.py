2f5f1d5bff49bec533356939d534be4e4901476d
Pedro Tashima <tithe.coup0l@icloud.com>
2026-05-12 16:47:40 -0300

    add stub implementation

--- kv_client/main.py
+++ kv_client/main.py
@@ -1,6 +1,16 @@
+import grpc
+import keyval_pb2
+import keyval_pb2_grpc
+
 def main():
-    print("Hello from kv-client!")
+    with grpc.insecure_channel('localhost:5431') as channel:
+        stub = keyval_pb2_grpc.KeyValStub(channel)
+        response = stub.Set(keyval_pb2.Pack(key="country", value="brasil"))
+        print("Greeter client received: " + response.value)
+        response = stub.Get(keyval_pb2.GetRequest(key='country'))
+        print("Greeter client received: " + response.value)
 
 
 if __name__ == "__main__":
     main()
+
